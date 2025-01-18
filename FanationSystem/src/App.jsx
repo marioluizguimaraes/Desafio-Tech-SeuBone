face88617743e226a63a10857fcbf62104dc22b4
mario <mariolsg.oficial@gmail.com>
2025-01-18 11:03:50 -0300

    ..

--- FanationSystem/src/App.jsx
+++ FanationSystem/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './components/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Login></Login>
     </>
   )
 }
---
+++ FanationSystem/src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import './Login.css'
+import LogoFanation from '../../assets/LogoFanation.png'
+
+export default function Login(){
+    return(
+        <div className="login">
+            <img src={LogoFanation} alt="logotipo fanation"/>
+
+        </div>
+    )
+}
